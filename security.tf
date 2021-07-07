ad26606250db7597279645f35fddb53538f3b5da
Ananya Jain <65167879+AnanyaJain111@users.noreply.github.com>
2021-07-07 18:40:37 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "SecGroup" {
+  name        = "TLS"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+  }
+
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+}
