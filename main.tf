e975c7a17332f8c776053070de1288e298ba295d
Ananya Jain <65167879+AnanyaJain111@users.noreply.github.com>
2021-07-07 18:39:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_key_pair" "terrakey" {
+  key_name   = "terraform-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
+resource "aws_instance" "os1" {
+ami = "ami-010aff33ed5991201"
+instance_type = "t2.micro"
+tags = {
+   Name = "My first OS"
+    }
+}
+
+output "ananya_os" {
+value = aws_instance.os1.availability_zone
+}
